0b64ad7b2d19136e04a205f73e7ad7302f59d485
EC2 Default User <ec2-user@ip-172-31-10-86.ap-northeast-1.compute.internal>
2023-02-09 09:26:25 +0000

    [Add] アソシエーション

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,7 +3,12 @@ class Public::OrdersController < ApplicationController
   end
   
   def create
-    
+    #@order = Order.new (order_params)
+    #@order.customer_id = current_customer_id
+    #if @order.save
+    #@order_detail = OrderDetail.new(item_id: ,order_id: ,@order.id,purchase_price: ,amount: ,is_active: )
+    #@order_detail.save
+    #end
   end
 
   def index
