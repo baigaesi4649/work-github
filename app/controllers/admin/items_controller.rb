5a2c7b5216ebd74c5801456f23de7ad4b9cd5225
EC2 Default User <ec2-user@ip-172-31-10-86.ap-northeast-1.compute.internal>
2023-02-12 23:35:14 +0000

    [Add] ジャンル編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,10 +12,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
-    @item =Item.find(params[:id])
+   @item =Item.find(params[:id])
   end
 
   def edit
+   @item =Item.find(params[:id])
   end
 
   def index
@@ -23,6 +24,13 @@ class Admin::ItemsController < ApplicationController
   end
   
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    #flash[:notice] = "You have updated user successfully."
+    redirect_to admin_item_path(@item.id)
+    else
+    render :edit
+    end
   end
 private
 
