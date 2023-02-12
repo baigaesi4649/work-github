c213e298a784db05d41d46644c9aab1013362572
EC2 Default User <ec2-user@ip-172-31-10-86.ap-northeast-1.compute.internal>
2023-02-12 09:05:42 +0000

    [Add] 管理者の商品詳細

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,12 @@
 class Admin::GenresController < ApplicationController
   def create
+       @genre=Genre.new(genre_params)
+    if @genre.save
+      #flash[:notice] = "You have updated user successfully."
+      redirect_to admin_item_path(:id)
+    else
+      render :admin_items_path
+    end
   end
   
   def index
@@ -11,4 +18,11 @@ class Admin::GenresController < ApplicationController
   
   def update
   end
+
+private
+def genre_params
+ params.require(:genre).permit(:name)
+end
+
+
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,18 @@
 class Admin::ItemsController < ApplicationController
   def new
    @item=Item.new
+   @items=Item.all
   end
   
   def create
+     @item=Item.new(item_params)
+     @item.save
+     #flash[:notice] = "You have updated user successfully."
+     redirect_to admin_item_path(@item.id)
   end
 
   def show
+    @item =Item.find(params[:id])
   end
 
   def edit
@@ -18,4 +24,10 @@ class Admin::ItemsController < ApplicationController
   
   def update
   end
+private
+
+def item_params
+ params.require(:item).permit(:genre_id,:name,:introduction,:price,:is_active)
+end
+
 end
