4835a1fecf5b552de6f2a2c7a668d9c0cdda1490
EC2 Default User <ec2-user@ip-172-31-10-86.ap-northeast-1.compute.internal>
2023-02-25 06:25:53 +0000

    レイアウト

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -4,17 +4,38 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = current_customer
+    @address = Address.find(params[:id])
   end
   
   def create
-    
+    @address = Address.new (address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+    #flash[:notice] = "You have created book successfully."
+    redirect_to public_addresses_path
+    end
   end
   
   def update
-    
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+    #flash[:notice] = "You have updated user successfully."
+    redirect_to public_addresses_path
+    else
+    render :edit
+    end
   end
   
   def destroy
-    
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to public_addresses_path
+  end
+  
+  private
+
+  def address_params
+    params.require(:address).permit(:name,:postal_code,:address)
   end
 end
