ef026fbe8b22d6d2ea18b13746ef375315bf3fa0
EC2 Default User <ec2-user@ip-172-31-10-86.ap-northeast-1.compute.internal>
2023-03-02 13:59:21 +0000

    [Finish] Work-github

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -30,7 +30,7 @@ class Public::SessionsController < Devise::SessionsController
     @customer = Customer.find_by(email: params[:customer][:email])
     return if !@customer
     if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
-      my_page_public_customers_path
+      public_customers_my_page_path
     else
       redirect_to new_customer_registration_path 
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,12 @@ class Item < ApplicationRecord
   has_many :order_detail, dependent: :destroy
   belongs_to :genre
   
-  def get_image
+  def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width,height]).processed
   end
   
   def with_tax_price
